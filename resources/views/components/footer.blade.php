50a2a1b4fe5b533c54ee9cf9807e6b1f48b989b6
unknown <user@1C1B0D604383.mck-ktits.ru>
2024-02-27 16:09:02 +0300

    x

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,12 +1,17 @@
 <footer class="border-t border-white border-opacity-25">
     <div class="container py-5 flex flex-wrap items-center justify-between gap-7">
-        <a href="{{ route('index.index') }}" class="text-2xl text-white"></a>
-        <ul class="flex flex-wrap items-center gap-14">
+        <a href="{{ route('index.index') }}"><img src="{{ asset('public/assets/images/logo.svg') }}" alt="Изображение" class="max-w-24 max-h-24"></a>
+        <ul class="items-center gap-14">
             <li><a class="h-5 hover:border-b border-white pb-2 transition" href="{{ route('index.index') }}">Главная</a></li>
             <li><a class="h-5 hover:border-b border-white pb-2 transition" href="{{ route('index.catalog') }}">Каталог</a></li>
             <li><a class="h-5 hover:border-b border-white pb-2 transition" href="#">О нас</a></li>
         </ul>
+        <ul class="items-center gap-14">
+            <li><a class="h-5 hover:border-b border-white pb-2 transition" href="{{ route('auth.loginPage') }}">Вход</a></li>
+            <li><a class="h-5 hover:border-b border-white pb-2 transition" href="{{ route('auth.registerPage') }}">Регистрация</a></li>
+        </ul>
         <div class="flex flex-wrap items-center gap-7">
+
         </div>
     </div>
 </footer>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <header class="border-b border-white border-opacity-25">
     <div class="container py-5 flex flex-wrap items-center justify-between gap-7">
-        <a href="{{ route('index.index') }}" class="text-2xl text-white">Ahmad strength</a>
+        <a href="{{ route('index.index') }}"><img src="{{ asset('public/assets/images/logo.svg') }}" alt="Изображение" class="max-w-24 max-h-24"></a>
         <ul class="flex flex-wrap items-center gap-14">
             <li><a class="h-5 hover:border-b border-white pb-2 transition" href="{{ route('index.index') }}">Главная</a></li>
             <li><a class="h-5 hover:border-b border-white pb-2 transition" href="{{ route('index.catalog') }}">Каталог</a></li>
